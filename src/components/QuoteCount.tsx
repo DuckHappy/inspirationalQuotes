62e9a0223e16851f9b42024c726107ca76fd7df1
vjaviertaype <vjaviertaype@outlook.com>
2025-08-19 20:17:42 -0300

    feat: quote-count creado usando contexto

--- src/components/QuoteCount.tsx
+++ src/components/QuoteCount.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
+import { useCacheContext } from "../context/cache";
+
 function QuoteCount() {
+  const { getItem, setItem } = useCacheContext();
+  const count = getItem("quote-count") || 0;
+
+  useEffect(() => {
+    setItem("quote-count", count + 1);
+  }, []);
+
   return (
     <div>
-      <span>Quote: {}</span>
+      <span>Quote count: {count}</span>
     </div>
   );
 }
